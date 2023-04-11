c80357a000abc7d8f988993319ff065c80a893f5
Clevins <cormaclevins@outlook.ie>
2023-04-11 23:47:17 +0100

    Fix phone scroll issue

--- src/lib/getSkills.ts
+++ src/lib/getSkills.ts
@@ -12,18 +12,6 @@ import JestIcon from "@assets/icons/tech/jest.svg";
 import TailwindIcon from "@assets/icons/tech/tailwind.svg";
 import NodeIcon from "@assets/icons/tech/node.svg";
 
-// const htmlIconUrl = "./src/assets/icons/tech/html5.svg";
-// const VueIconUrl = "./src/assets/icons/tech/css.svg";
-// const tsIconUrl = "./src/assets/icons/tech/ts.svg";
-// const reactIconUrl = "./src/assets/icons/tech/react.svg";
-// const nextjsIconUrl = "./src/assets/icons/tech/nextjs.svg";
-
-// const svelteIconUrl = "./src/assets/icons/tech/svelte.svg";
-// const vueIconUrl = "./src/assets/icons/tech/vue.svg";
-// const jestIconUrl = "./src/assets/icons/tech/jest.svg";
-// const tailwindIconUrl = "./src/assets/icons/tech/tailwind.svg";
-// const nodeIconUrl = "./src/assets/icons/tech/node.svg";
-
 const getSkills = [
   new Skill("HTML 5", "HTML 5", HtmlIcon),
   new Skill("CSS3", "CSS3", CssIcon),
