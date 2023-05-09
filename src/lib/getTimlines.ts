84dbc2254431d6df2ea10194665dd7524c20d095
Clevins <cormaclevins@outlook.ie>
2023-05-09 20:12:04 +0100

    Change education timeline

--- src/lib/getTimlines.ts
+++ src/lib/getTimlines.ts
@@ -4,8 +4,8 @@ import CTimeline from "./Classes/Timeline";
 export const getTimelines = () => {
   return [
     new CTimeline("Education", [
-      new CEntry("St.Joseph's School", "High School ", "2011", "2016"),
       new CEntry("DKIT", "Bsc In Computing", "2016", "2020"),
+      new CEntry("St.Joseph's School", "High School ", "2011", "2016"),
     ]),
     new CTimeline("Work History", [
       new CEntry("Axonista", "Web Developer", "2020", "2023"),
