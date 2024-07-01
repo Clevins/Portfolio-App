8bd26a1b3202bb6b0be613a00a30fe0d058dd43a
Clevins <cormaclevins@outlook.ie>
2024-07-01 14:04:59 +0100

    update profile section

--- src/components/About/Profile/Profile.tsx
+++ src/components/About/Profile/Profile.tsx
@@ -11,23 +11,23 @@ const Profile: FC = () => {
       </div>
       <div className={styles.profile}>
         <p>
-          Hi, my name is Cormac Levins, I am a web developer and I've recently
-          relocated to Vancouver and am looking for a new opportunity. I started
-          my journey when I enrolled in Bsc. In Computing at Dundalk Institute
-          for Technology. After a few days, I knew this is what I wanted to do
-          as a career and I have been working at it ever since. Over the last
-          three years, I have worked with several companies in Ireland, the
-          Netherlands, and the US. And have worked with team members across the
-          globe in a remote enviroment building multimedia web apps.
+          Hi, my name is Cormac Levins. I am a web developer based in Dublin,
+          Ireland and I am actively seeking new opportunities. My journey began
+          when I enrolled in the BSc in Computing program at Dundalk Institute
+          of Technology in Ireland. Within a few days, I knew this was the
+          career I wanted to pursue. Four years later, I graduated with a First
+          Class Honours degree and have been dedicated to building my career and
+          enhancing my skills ever since.
         </p>
 
         <p>
-          I believe every small detail matters. I prioritize the quality of my
-          code over most other things, It's rarely perfect but I am always
-          making a conscious effort to improve it consistently as the project
-          goes on. I feel that writing clean and fully maintainable code is the
-          most important factor in reducing and eliminating technical dept and
-          have made this a primary focus in recent projects.
+          Over the years, I've had the privilege of working with several amazing
+          companies in Ireland, the Netherlands, Canada, and the US. I've
+          collaborated with brilliant team members worldwide in remote
+          environments, focusing on building multimedia web apps. My top skills
+          include JavaScript, TypeScript, React.js, Svelte, and Vue.js. I am
+          passionate about web development and am always looking for new
+          challenges and opportunities to grow.
         </p>
       </div>
     </>
--- src/lib/getDetails.ts
+++ src/lib/getDetails.ts
@@ -4,7 +4,7 @@ const getDetails = () => {
   return [
     { label: "Birthday", body: "11.06.1998" },
     { label: "Email", body: "cormaclevins@outlook.ie" },
-    { label: "Phone", body: "(+1) 236-867-1166" },
+    { label: "Phone", body: "(+353) 86 209 8913 " },
     { label: "Nation", body: "Ireland" },
     { label: "Degree", body: "BSc In Computing" },
     { label: "Interest", body: "Reading & Cooking" },
