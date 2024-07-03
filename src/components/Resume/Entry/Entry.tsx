fefafa6431110177308324982c5480012407a24e
Clevins <cormaclevins@outlook.ie>
2024-07-03 13:40:33 +0100

    update Entry for new timeline

--- src/components/Resume/Entry/Entry.tsx
+++ src/components/Resume/Entry/Entry.tsx
@@ -6,24 +6,44 @@ import styles from "./Entry.module.css";
 
 const Entry: FC<EntryProps> = ({ data }) => {
   return (
-    <div>
-      <div className={styles.line} />
-      <div className={styles.container}>
-        <div className={styles.entry}>
-          <div className={styles.entry__circle}></div>
+    <div className={styles.container}>
+      <div className={styles.entry}>
+        {data !== null ? (
           <div className={styles.entry__year}>
             <Button
               isDisabled={true}
             >{`${data.startDate} - ${data.endDate} `}</Button>
           </div>
-          <div className={styles.details}>
-            <div className={styles.details__heading}>{data.title}</div>
-            <div className={styles.details__subheading}>{data.subTitle}</div>
-          </div>
-        </div>
+        ) : (
+          <div></div>
+        )}
+        {/* <div className={styles.entry__circle}></div> */}
+
+        {/* <div className={styles.details}>
+          <div className={styles.details__heading}>{data.title}</div>
+          <div className={styles.details__subheading}>{data.subTitle}</div>
+        </div> */}
       </div>
     </div>
   );
 };
 
 export default Entry;
+
+{
+  /* <div className={styles.container}>
+        <div className={styles.container__circle}></div>
+        <div className={styles.entry}>
+          <div className={styles.entry__year}>
+            <Button isDisabled={true}>{`2016 - 2020 `}</Button>
+          </div>
+          <div className={styles.details}>
+            <div className={styles.details__heading}>St. Joesphas School</div>
+            <div className={styles.details__subheading}>Secondary School</div>
+            <div className={styles.details__subheading}>
+              Received my leaving Cert Certificate{" "}
+            </div>
+          </div>
+        </div>
+      </div> */
+}
--- src/lib/Classes/Entry.ts
+++ src/lib/Classes/Entry.ts
@@ -1,17 +1,20 @@
 export default class CEntry {
   title: string;
   subTitle: string;
+  description: string;
   startDate: string;
   endDate: string;
 
   constructor(
     title: string,
     subTitle: string,
+    description: string,
     startDate: string,
     endDate: string
   ) {
     this.title = title;
     this.subTitle = subTitle;
+    this.description = description;
     this.startDate = startDate;
     this.endDate = endDate;
   }
